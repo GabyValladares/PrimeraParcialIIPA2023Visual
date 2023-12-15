7053b934845186dcb62ba905c3842aacd510c014
LeonardoMontalvo <axel.montalvo186@ist17dejulio.edu.ec>
2023-12-15 15:55:36 -0500

    Examen

--- src/DatosVehiculo.java
+++ src/DatosVehiculo.java
@@ -14,6 +14,7 @@ public class DatosVehiculo extends javax.swing.JFrame {
      */
     public DatosVehiculo() {
         initComponents();
+       
     }
 
     /**
@@ -54,6 +55,9 @@ public class DatosVehiculo extends javax.swing.JFrame {
         jScrollPane9 = new javax.swing.JScrollPane();
         txtMultas = new javax.swing.JTextPane();
         lblTema = new javax.swing.JLabel();
+        btnValidar = new javax.swing.JButton();
+        jScrollPane10 = new javax.swing.JScrollPane();
+        txtValidacion = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -105,12 +109,30 @@ public class DatosVehiculo extends javax.swing.JFrame {
         lblTema.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         lblTema.setText(" Vehículo a matricular");
 
+        btnValidar.setText("VALIDAR");
+        btnValidar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnValidarActionPerformed(evt);
+            }
+        });
+
+        txtValidacion.setColumns(20);
+        txtValidacion.setRows(5);
+        jScrollPane10.setViewportView(txtValidacion);
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(btnValidar)
+                .addGap(421, 421, 421))
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(203, 203, 203)
+                        .addComponent(lblTema))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(18, 18, 18)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -134,11 +156,10 @@ public class DatosVehiculo extends javax.swing.JFrame {
                             .addComponent(jScrollPane6)
                             .addComponent(jScrollPane7)
                             .addComponent(jScrollPane8)
-                            .addComponent(jScrollPane9)))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(203, 203, 203)
-                        .addComponent(lblTema)))
-                .addContainerGap(64, Short.MAX_VALUE))
+                            .addComponent(jScrollPane9))
+                        .addGap(91, 91, 91)
+                        .addComponent(jScrollPane10, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(133, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -153,15 +174,21 @@ public class DatosVehiculo extends javax.swing.JFrame {
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(lblNCedula)
                                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addGap(24, 24, 24)
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(lblNombres)
-                                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addGap(30, 30, 30)
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(lblPlaca)
-                                    .addComponent(jScrollPane5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addGap(33, 33, 33)
+                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addGap(24, 24, 24)
+                                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                            .addComponent(lblNombres)
+                                            .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                        .addGap(36, 36, 36)
+                                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                            .addComponent(lblPlaca)
+                                            .addComponent(jScrollPane5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                        .addGap(33, 33, 33))
+                                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(jScrollPane10, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addGap(15, 15, 15)))
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(lblAnioFabricacion)
                                     .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -183,7 +210,9 @@ public class DatosVehiculo extends javax.swing.JFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(jScrollPane9, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(lblMultas))
-                .addContainerGap(35, Short.MAX_VALUE))
+                .addGap(26, 26, 26)
+                .addComponent(btnValidar)
+                .addContainerGap(39, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -194,11 +223,66 @@ public class DatosVehiculo extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnValidarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnValidarActionPerformed
+    
+ String cedula = txtCedula.getText();
+    String propietario = txtNombres.getText();
+    String placa = txtPlaca.getText();
+    String añoFabricacion = txtAnio.getText();
+    String marca = txtMarca.getText();
+    String color = txtColor.getText();
+    String tipoVehiculo = txtTipo.getText();
+    String valor = txtValor.getText();
+    String multas = txtMultas.getText();
+
+    int añoFabricacionInt = Integer.parseInt(añoFabricacion);
+    double valorVehiculo = Double.parseDouble(valor);
+    boolean tieneMultas = !multas.isEmpty();
+
+    double importeRenovacionPlacas = 0;
+    double multaContaminacion = 0;
+    double valorMatriculacion = 0;
+
+    if (cedula.startsWith("1") && placa.startsWith("I")) {
+        importeRenovacionPlacas = 0.05 * 435; 
+    }
+
+    if (añoFabricacionInt < 2010) {
+        multaContaminacion = 0.02 * (2022 - añoFabricacionInt); 
+    }
+
+    if (marca.equalsIgnoreCase("Toyota")) {
+        if (tipoVehiculo.equalsIgnoreCase("Jeep")) {
+            valorMatriculacion = 0.08 * valorVehiculo;
+        } else if (tipoVehiculo.equalsIgnoreCase("Camioneta")) {
+            valorMatriculacion = 0.12 * valorVehiculo;
+        }
+    } else if (marca.equalsIgnoreCase("Suzuki")) {
+        if (tipoVehiculo.equalsIgnoreCase("Vitara")) {
+            valorMatriculacion = 0.10 * valorVehiculo;
+        } else if (tipoVehiculo.equalsIgnoreCase("Automóvil")) {
+            valorMatriculacion = 0.09 * valorVehiculo;
+        }
+    }
+
+    double multaPorMultas = tieneMultas ? 0.25 * 435 : 0;
+
+    double totalPagar = importeRenovacionPlacas + multaContaminacion + valorMatriculacion + multaPorMultas;
+
+    txtValidacion.setText("Importe Renovación Placas: $" + importeRenovacionPlacas +
+    "\nMulta Contaminación: $" + multaContaminacion +
+    "\nValor Matriculación: $" + valorMatriculacion +
+    "\nMulta por Multas: $" + multaPorMultas +
+    "\nTotal a Pagar: $" + totalPagar);
+    }//GEN-LAST:event_btnValidarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -236,8 +320,10 @@ public class DatosVehiculo extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnValidar;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane10;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JScrollPane jScrollPane3;
     private javax.swing.JScrollPane jScrollPane4;
@@ -264,6 +350,7 @@ public class DatosVehiculo extends javax.swing.JFrame {
     private javax.swing.JTextPane txtNombres;
     private javax.swing.JTextPane txtPlaca;
     private javax.swing.JTextPane txtTipo;
+    private javax.swing.JTextArea txtValidacion;
     private javax.swing.JTextPane txtValor;
     // End of variables declaration//GEN-END:variables
 }

fe869bb9b9dfdeaae24a28db0de5c7f05e8bd46e
CG774 <Carlos2004Gordillo@gmail.com>
2023-12-15 13:59:54 -0500

    Formulario de datos

--- src/clases/ValidacionMatricula.java
+++ src/clases/ValidacionMatricula.java
@@ -8,12 +8,12 @@ package clases;
  *
  * @author Usuario
  */
-public class Ficha extends javax.swing.JFrame {
+public class ValidacionMatricula extends javax.swing.JFrame {
 
     /**
      * Creates new form Ficha
      */
-    public Ficha() {
+    public ValidacionMatricula() {
         initComponents();
     }
 
@@ -59,20 +59,21 @@ public class Ficha extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Ficha.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ValidacionMatricula.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Ficha.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ValidacionMatricula.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Ficha.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ValidacionMatricula.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Ficha.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ValidacionMatricula.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Ficha().setVisible(true);
+                new ValidacionMatricula().setVisible(true);
             }
         });
     }
